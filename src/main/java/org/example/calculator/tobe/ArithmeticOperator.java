52961df2327c445183a547bdf8710d20e30ef6c2
JaeHoon <wogns8030@naver.com>
2024-01-11 19:52:37 +0900

    push

--- src/main/java/org/example/calculator/tobe/ArithmeticOperator.java
+++ src/main/java/org/example/calculator/tobe/ArithmeticOperator.java
@@ -4,5 +4,6 @@ import org.example.calculator.domain.PositiveNumber;
 
 public interface ArithmeticOperator {
     boolean supports(String operator);
+
     int calculate(final PositiveNumber operand1, final PositiveNumber operand2);
 }
